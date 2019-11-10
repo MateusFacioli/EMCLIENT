48c563f3dac973cb1221077b7203df01510f5a16
leandrx11 <dellg7gaming>
2019-11-09 22:17:53 -0300

    Alteração mensagem de erro no login

--- app/src/main/java/com/izi/tcccliente/activity/LoginActivity.java
+++ app/src/main/java/com/izi/tcccliente/activity/LoginActivity.java
@@ -104,7 +104,7 @@ public class LoginActivity extends AppCompatActivity {
                         } else {
                             // If sign in fails, display a message to the user.
                             Log.w("TagLoginErrado", "Email ou Senha invalidos!", task.getException());
-                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                            Toast.makeText(LoginActivity.this, "Erro! E-mail ou senha inválidos",
                                     Toast.LENGTH_SHORT).show();
 
                         }
